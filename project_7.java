57742983544a1f6647527d2cbfb46aa76c95b6a6
surbhi-gits <92331117+surbhi-gits@users.noreply.github.com>
2021-11-15 21:30:27 -0800

    print_message

---
+++ project_7.java
@@ -0,0 +1,13 @@
+package com.company;
+class programming{
+    programming(String a){
+        System.out.println("I love "+a);
+    }
+}
+public class project_7 {
+    public static void main(String[] args) {
+        String a="Surbhi";
+        programming m=new programming(a);
+    }
+}
+
